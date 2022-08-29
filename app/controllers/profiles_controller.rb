fa25357d48d944f2aea451f19e3943b33ba4b6ec
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-29 17:16:24 +0200

    work in progress

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,23 @@
 class ProfilesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def 
 end
