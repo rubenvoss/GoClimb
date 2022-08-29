f8d9c1c920802ac7c9888daf59c409f9d131a86e
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-29 14:45:01 +0200

    activity model

---
+++ db/migrate/20220829124446_create_activities.rb
@@ -0,0 +1,13 @@
+class CreateActivities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.float :lat
+      t.float :long
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
