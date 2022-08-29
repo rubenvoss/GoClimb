75b3d87c9d5f1156f73e3d448c4ca6c5e375f617
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-29 14:37:41 +0200

    review model

---
+++ db/migrate/20220829123732_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.text :comment
+      t.references :user, null: false, foreign_key: true
+      t.references :profile, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
