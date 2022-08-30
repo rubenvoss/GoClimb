effbea59746818e1aa1df9de815c228b428a61df
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-30 14:38:54 +0200

    working on controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,41 @@
 class ReviewsController < ApplicationController
+  def index
+    @profile = Profile.find(params[:profile_id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.crag = Crag.find(profile_params[:crag_id])
+    @profile.user = current_user
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @crag = @profile.crag
+    @profile.user = current_user
+    if @profile.update(profile_params)
+      redirect_to profile_path(@profile)
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :bio, :crag_id)
+  end
 end
