02d13d8a4554b11cabcb8df47632a4c9c5791e63
Ruben Voss <bekertshop@gmail.com>
2022-08-30 13:30:55 +0200

    working on seeds

--- Gemfile
+++ Gemfile
@@ -57,7 +57,7 @@ gem "devise"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
-
+gem "cloudinary"
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-crags = [
+crags_array = [
   {
-            image: "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661856531/go-climb-seeds/margalef-1_dka26r.jpg"
-
+    name: "Margalef",
+    photo_key: "margalef-1_dka26r",
+    lat: 1,
+    long: 2
           }
-
-
 ]
+
+crags.each_with_index do |crag, index|
+  Crag.create(crags_array[n])
+end
