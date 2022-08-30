01765cc30fa33186e29592ce20cd0d85e1f5c782
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-30 14:33:03 +0200

    added nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :profiles, except: :index
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :profiles, except: %i[index destroy] do
+    member do
+      resources :reviews, except: %i[show destroy]
+    end
+  end
 end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Defines the root path route ("/")
+# root "articles#index"
