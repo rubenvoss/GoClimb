e6102fc4ba8ad84d2f116b96fae3ab22c20d1083
Ruben Voss <bekertshop@gmail.com>
2022-08-30 14:26:44 +0200

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,41 @@
-crags_array = [
-  {
-    name: "Margalef",
-    photo_key: "margalef-1_dka26r",
-    lat: 1,
-    long: 2
-          }
-]
+require "open-uri"
 
-crags.each_with_index do |crag, index|
-  Crag.create(crags_array[n])
+puts "cleaning dataase..."
+Country.destroy_all
+Crag.destroy_all
+User.destroy_all
+Message.destroy_all
+Chatroom.destroy_all
+Trip.destroy_all
+Activity.destroy_all
+Review.destroy_all
+Profile.destroy_all
+
+# all of the data for the seeds
+crags_data = [
+                {
+                  name: "Margalef",
+                  lat: 1,
+                  long: 2,
+                  country: Country.where(name: "Spain")
+                }
+            ]
+crags_photos = { Margalef: "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661856531/go-climb-seeds/margalef-1_dka26r.jpg"
+               }
+
+
+#
+#
+#
+# seeding the data
+puts "seeding countries..."
+Country.create(name: "Spain")
+Country.create(name: "Germany")
+
+puts "seeding crags..."
+crags_data.each do |crag_data|
+  crag = Crag.new(crag_data)
+  photo = URI.open(crags_photos[crag.name])
+  crag.photo.attach(io: photo, filename: "#{crag.name}.jpg", content_type: "image/jpg")
+  crag.save!
 end
