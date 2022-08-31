9815868a133c8a6e9d7d4da867b3c8eed3a2468b
Ruben Voss <bekertshop@gmail.com>
2022-08-31 12:40:25 +0200

    added pg_search to models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,8 @@
 class Country < ApplicationRecord
   has_many :crags
   validates :name, presence: true
+
+  # makes countries searchable for names
+  include PgSearch::Model
+  multisearchable against: [:name]
 end
--- app/models/crag.rb
+++ app/models/crag.rb
@@ -3,4 +3,8 @@ class Crag < ApplicationRecord
   has_many :profiles
   validates :name, :lat, :long, presence: true
   has_one_attached :photo
+
+  # makes crags searchable for names
+  include PgSearch::Model
+  multisearchable against: [:name]
 end
