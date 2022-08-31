8bcbf9fb8134337f3c6382cfb0eec6a7bca62184
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-31 11:29:40 +0200

    controller methods

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,24 @@
 class ChatroomsController < ApplicationController
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  
+
   resources :profiles, except: %i[index destroy] do
     resources :reviews, only: %i[new create]
   end
-  
+
   resources :reviews, only: [:destroy]
   resources :crags
+  resources :chatrooms, only: %i[show new create]
 end
