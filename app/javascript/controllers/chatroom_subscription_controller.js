c431a8079c76ec9d4bd667b01904c147b524a39c
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-31 14:11:48 +0200

    check controller connection

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
   connect() {
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
