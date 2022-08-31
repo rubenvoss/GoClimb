46b48a36cb849090aa5d83725e9066f28d57baba
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-31 18:04:39 +0200

    js chatbox working

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,8 +4,8 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
   static targets = ["messages"]
+  static values = { chatroomId: Number, currentUserId: Number }
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
@@ -14,10 +14,26 @@ export default class extends Controller {
     )
   }
 
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${currentUserIsSender ? "justify-content-end" : "justify-content-start"}">
+        <div class="message-style">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    // Creating the whole message from the `data.message` String
+    // this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
+
 
   resetForm(event) {
     event.target.reset()
