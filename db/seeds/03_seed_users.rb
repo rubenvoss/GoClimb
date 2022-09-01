9b6748d3934816caf87cf56f5c4656ee2333cde2
Ruben Voss <bekertshop@gmail.com>
2022-09-01 21:58:29 +0200

    working on profiles

--- db/seeds/03_seed_users.rb
+++ db/seeds/03_seed_users.rb
@@ -1,13 +1,11 @@
 def seed_users
-  5.times do
+  10.times do |index|
     User.create ({
-      male_first_name: Faker::Name.male_first_name,
-      last_name: Faker::Name.last_name,
-      crag: Crag.all.sample
+      email: "user@user#{index}",
+      password: "123456"
     })
   end
 end
-
 
 
 
--- db/seeds/04_seed_profiles.rb
+++ db/seeds/04_seed_profiles.rb
@@ -1,46 +1,61 @@
-
-
-
-
-
-male_profile_photos = [
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662031176/go%20climb%20profile%20pictures/omid-armin-D9RrI5IW9h0-unsplash_50_1_50_joecin.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662031008/go%20climb%20profile%20pictures/tommy-lisbin-zUHe9T8Zsj8-unsplash_50_1_50_kxzwdn.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030113/go%20climb%20profile%20pictures/tommy-lisbin-tl88ay2QxLU-unsplash_50_1_50_zjqcmw.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662029774/go%20climb%20profile%20pictures/petr-slovacek-YqVDdDkdFkw-unsplash_50_1_50_md4jrj.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030258/go%20climb%20profile%20pictures/mihajlo-sebalj-swfJN1URFzA-unsplash_50_whpy63.jpg"
+male_profiles = [
+  {
+    name: "Peter Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662031176/go%20climb%20profile%20pictures/omid-armin-D9RrI5IW9h0-unsplash_50_1_50_joecin.jpg"
+  },
+  {
+    name: "Peter Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662030258/go%20climb%20profile%20pictures/mihajlo-sebalj-swfJN1URFzA-unsplash_50_whpy63.jpg"
+  },
+  {
+    name: "Peter Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662029774/go%20climb%20profile%20pictures/petr-slovacek-YqVDdDkdFkw-unsplash_50_1_50_md4jrj.jpg"
+  },
+  {
+    name: "Peter Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662030113/go%20climb%20profile%20pictures/tommy-lisbin-tl88ay2QxLU-unsplash_50_1_50_zjqcmw.jpg"
+  },
+  {
+    name: "Peter Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662031008/go%20climb%20profile%20pictures/tommy-lisbin-zUHe9T8Zsj8-unsplash_50_1_50_kxzwdn.jpg"
+  }
 ]
 
-female_profile_photos = [
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030864/go%20climb%20profile%20pictures/elijah-hiett-ai7tP-odA2c-unsplash_1_50_e8pfqj.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030692/go%20climb%20profile%20pictures/jason-gardner-el88SU3-AQY-unsplash_50_f0o5w7.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030519/go%20climb%20profile%20pictures/frances-gunn-fUpgJKCs1fw-unsplash_50_1_50_tzdwwj.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662029941/go%20climb%20profile%20pictures/emma-smith-9Bv0OzPxcOI-unsplash_50_sgqmmp.jpg",
-  "https://res.cloudinary.com/dqdezmage/image/upload/v1662028670/go%20climb%20profile%20pictures/khamkhor--I-McziCxxM-unsplash_50_wnumu1.jpg"
+female_profiles = [
+  {
+    name: "Maria Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662028670/go%20climb%20profile%20pictures/khamkhor--I-McziCxxM-unsplash_50_wnumu1.jpg"
+  },
+  {
+    name: "Maria Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662029941/go%20climb%20profile%20pictures/emma-smith-9Bv0OzPxcOI-unsplash_50_sgqmmp.jpg"
+  },
+  {
+    name: "Maria Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662030519/go%20climb%20profile%20pictures/frances-gunn-fUpgJKCs1fw-unsplash_50_1_50_tzdwwj.jpg"
+  },
+  {
+    name: "Maria Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662030692/go%20climb%20profile%20pictures/jason-gardner-el88SU3-AQY-unsplash_50_f0o5w7.jpg"
+  },
+  {
+    name: "Maria Müller",
+    url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662030864/go%20climb%20profile%20pictures/elijah-hiett-ai7tP-odA2c-unsplash_1_50_e8pfqj.jpg"
+  }
 ]
 
-puts "seeding male users and profiles..."
-male_users_data = []
-crags = Crag.all
-
-
-male_users = []
-male_profiles = []
-index = 0
-male_users_data.each do |male_user_data|
-  user = User.create(
-    email: "#{male_user_data[:male_first_name]}.#{male_user_data[:last_name]}@gmail.com",
-    password: "123456"
-  )
-  male_users << user
-  puts "male user with id #{user.id} created"
-  male_profile = Profile.create(name: male_user_data[:male_first_name], crag: male_user_data[:crag], user: user)
-  male_profiles << male_profile
-  photo = URI.open(male_profile_photos[index])
-  male_profile.photo.attach(io: photo, filename: "photo.jpg", content_type: "image/jpg")
-  puts "male profile for #{male_user_data[:male_first_name]} with id #{male_profile.id} created"
-  index += 1
+def seed_male_users(male_profiles_data)
+  male_profiles.each_with_index do |male_profile, index|
+    profile = Profile.new(name: male_profile[:name], crag: Crag.all.sample, user: User.find(index + 1))
+    photo = URI.open(male_profile[:url])
+    profile.photo.attach(io: photo, filename: "photo.jpg", content_type: "image/jpg")
+    puts "male profile for #{profile.name} with id #{profile.id} created"
+  end
 end
+
+seed_male_users(male_profiles_data)
+
+
 
 puts "seeding female users and profiles..."
 female_users_data = []
