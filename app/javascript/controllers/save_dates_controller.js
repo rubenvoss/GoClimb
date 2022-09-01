213b48abba5be0b6fe5b502f87786169862c017a
Ruben Voss <bekertshop@gmail.com>
2022-09-01 16:36:46 +0200

    connected save-dates controller

--- app/javascript/controllers/save_dates_controller.js
+++ app/javascript/controllers/save_dates_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="save-dates"
 export default class extends Controller {
   connect() {
+    console.log("save dates controller is connected")
   }
 }
