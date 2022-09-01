4234c1b6f6cd020b53f69dac6ebd5a88f18bacc3
Ruben Voss <bekertshop@gmail.com>
2022-09-01 22:58:55 +0200

    seeding 5 chatrooms

--- db/seeds/05_chatroom_seed.rb
+++ db/seeds/05_chatroom_seed.rb
@@ -1,6 +1,10 @@
-# puts "seeding chatrooms..."
-# 5.times do
-#   name = Profile.all.sample.name
-#   Chatroom.create(name: name)
-#   puts "chatroom with name #{name} created"
-# end
+def seed_chatrooms
+  puts "seeding chatrooms..."
+  5.times do
+    name = Profile.all.sample.name
+    Chatroom.create(name: name)
+    puts "chatroom with name #{name} created"
+  end
+end
+
+seed_chatrooms
