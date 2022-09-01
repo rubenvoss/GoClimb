7afdaadd4fc4e1b2453fbe8732e3e56503bc6337
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-01 13:30:26 +0200

    added profile pictures, optimized seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,19 +96,65 @@ crags_data.each do |crag_data|
   Chatroom.create(name: chatroom_name)
 end
 
-puts "seeding users..."
-10.times do
-  User.create(email: "#{Faker::Name.first_name}@gmail.com", password: Faker::Alphanumeric.alphanumeric(number: 10))
+# IMPORTANT: the symbol has to match exactly the crag name in crags_data
+male_profile_photos = [
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662031176/go%20climb%20profile%20pictures/omid-armin-D9RrI5IW9h0-unsplash_50_1_50_joecin.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662031008/go%20climb%20profile%20pictures/tommy-lisbin-zUHe9T8Zsj8-unsplash_50_1_50_kxzwdn.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030113/go%20climb%20profile%20pictures/tommy-lisbin-tl88ay2QxLU-unsplash_50_1_50_zjqcmw.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662029774/go%20climb%20profile%20pictures/petr-slovacek-YqVDdDkdFkw-unsplash_50_1_50_md4jrj.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030258/go%20climb%20profile%20pictures/mihajlo-sebalj-swfJN1URFzA-unsplash_50_whpy63.jpg"
+]
+
+female_profile_photos = [
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030864/go%20climb%20profile%20pictures/elijah-hiett-ai7tP-odA2c-unsplash_1_50_e8pfqj.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030692/go%20climb%20profile%20pictures/jason-gardner-el88SU3-AQY-unsplash_50_f0o5w7.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662030519/go%20climb%20profile%20pictures/frances-gunn-fUpgJKCs1fw-unsplash_50_1_50_tzdwwj.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662029941/go%20climb%20profile%20pictures/emma-smith-9Bv0OzPxcOI-unsplash_50_sgqmmp.jpg",
+  "https://res.cloudinary.com/dqdezmage/image/upload/v1662028670/go%20climb%20profile%20pictures/khamkhor--I-McziCxxM-unsplash_50_wnumu1.jpg"
+]
+
+puts "seeding users and profiles..."
+male_users = 5.times do
+  {
+    male_first_name: Faker::Name.male_first_name,
+    last_name: Faker::Name.last_name
+  }
 end
 
-puts "seeding profiles..."
-users = User.all
+puts "5 male persons created"
+
 crags = Crag.all
-crags.each do |crag|
-  users.each do |user|
-    Profile.create(name: Faker::Name.first_name, crag: crag, user: user)
+male_users.each do |male_user|
+  user = User.create(email: "#{male_first_name}.#{last_name}@gmail.com", password: Faker::Alphanumeric.alphanumeric(number: 10))
+  crags.each do |crag|
+    male_profile_photos.each do |male_photo|
+      Profile.create(name: male_user[:male_first_name], crag: crag, user: user, photo: male_photo)
+    end
   end
 end
+
+puts "5 male users and profiles created"
+
+female_users = 5.times do
+  {
+    female_first_name: Faker::Name.female_first_name,
+    last_name: Faker::Name.last_name
+  }
+end
+
+puts "5 female persons created"
+
+crags = Crag.all
+female_users.each do |female_user|
+  user = User.create(email: "#{female_first_name}.#{last_name}@gmail.com", password: Faker::Alphanumeric.alphanumeric(number: 10))
+  crags.each do |crag|
+    female_profile_photos.each do |female_photo|
+      Profile.create(name: female_user[:female_first_name], crag: crag, user: user, photo: female_photo)
+    end
+  end
+end
+
+puts "5 female users and profiles created"
 
 # seeding messages only for checking layout purposes in development
 
