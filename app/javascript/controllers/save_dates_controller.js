1bb0ea54dc7e9fae3f4a6984c8a91c2dd45f6091
Ruben Voss <bekertshop@gmail.com>
2022-09-02 12:36:31 +0200

    storage persists

--- app/javascript/controllers/save_dates_controller.js
+++ app/javascript/controllers/save_dates_controller.js
@@ -3,22 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="save-dates"
 export default class extends Controller {
   continue() {
-    event.preventDefault();
-    console.log("you pressed submit")
-    // save trip_start_date
-    // save trip_end_date
-    // preventDefault
-    // let tripEndDate = document.getElementById('search_trip_end_date')
-    // let tripStartDate = document.getElementById('search_trip_start_date')
-    this.startDate = flatpickr(search_trip_start_date, {}).selectedDates
-    this.endDate = flatpickr(search_trip_end_date, {}).selectedDates
-    console.log(this.startDate[0])
-    console.log(this.endDate[0])
+    let startDate = flatpickr(search_trip_start_date, {}).selectedDates
+    let endDate = flatpickr(search_trip_end_date, {}).selectedDates
+    localStorage.setItem("startDate", startDate)
+    localStorage.setItem("endDate", endDate)
   }
 
   show() {
-    console.log(this)
-    console.log(this.startDate)
-    console.log(this.endDate)
+    const startDate = localStorage.getItem('startDate');
+    const endDate = localStorage.getItem('endDate');
+    console.log(startDate)
+    console.log(endDate)
   }
 }
