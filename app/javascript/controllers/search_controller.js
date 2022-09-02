ba6a02cb6428728b2d50c294af6db1ab71cf2b0d
Ruben Voss <bekertshop@gmail.com>
2022-09-02 15:19:12 +0200

    passing start_date end_date in the params

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = ["input", "list", "form"]
+  static targets = ["start", "end", "input", "list", "form"]
   update() {
     // console.log("value: " + this.inputTarget.value)
     // console.log("list: " + this.listTarget)
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}&start_date=${this.startTarget.value}&end_date=${this.endTarget.value}`
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
