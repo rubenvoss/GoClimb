235f7478891059bf60b23e8d740146577543103d
Ruben Voss <bekertshop@gmail.com>
2022-09-03 18:16:14 +0200

    removed unnecessary passing to the params

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = ["start", "end", "input", "list", "form"]
+  static targets = ["input", "list", "form"]
   update() {
     // console.log("value: " + this.inputTarget.value)
     // console.log("list: " + this.listTarget)
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}&start_date=${this.startTarget.value}&end_date=${this.endTarget.value}`
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
@@ -15,5 +15,4 @@ export default class extends Controller {
         // console.log(data)
       })
   }
-
 }
