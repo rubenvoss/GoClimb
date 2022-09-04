d13dd6d20fa92bd4861f470935bd94367b526191
Ruben Voss <bekertshop@gmail.com>
2022-09-04 13:47:26 +0200

    implemented ajax request on crag#show

--- app/javascript/controllers/save_dates_controller.js
+++ app/javascript/controllers/save_dates_controller.js
@@ -3,7 +3,21 @@ import { end } from "@popperjs/core"
 
 // Connects to data-controller="save-dates"
 export default class extends Controller {
-  static targets = ["heading"]
+  static targets = ["travelling_climbers"]
+
+  displayTravellingClimbers() {
+    fetch(this.travelling_climbersTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
+
   // gets triggered when the user clicks the continue button
   continue() {
     let startDate = flatpickr(search_trip_start_date, {}).selectedDates
@@ -12,5 +26,6 @@ export default class extends Controller {
     localStorage.setItem("endDate", endDate)
     console.log(startDate)
     console.log(endDate)
+    this.displayTravellingClimbers()
   }
 }
