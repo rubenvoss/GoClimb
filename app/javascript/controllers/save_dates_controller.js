f22dcd10ea4f555e44e3b55e476804dabc15d6ed
Ruben Voss <bekertshop@gmail.com>
2022-09-04 14:55:17 +0200

    working on rendering json in crag#show

--- app/javascript/controllers/save_dates_controller.js
+++ app/javascript/controllers/save_dates_controller.js
@@ -6,15 +6,23 @@ export default class extends Controller {
   static targets = ["travelling_climbers"]
 
   displayTravellingClimbers() {
-    fetch(this.travelling_climbersTarget.action, {
-      method: "POST",
-      headers: { "Accept": "application/json" },
-      body: new FormData(this.formTarget)
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-      })
+    // fetch(
+    //   "http://localhost:3000/crags/7"
+    //   // this.travelling_climbersTarget.action
+    //   // ,
+    // //   {
+    // //   method: "POST",
+    // //   headers: { "Accept": "application/json" },
+    // //   body: new FormData(this.formTarget)
+    // // }
+    // )
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //   })
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
   }
 
 
---
+++ app/views/crags/show.json.jbuilder
@@ -0,0 +1,2 @@
+json.inserted_item render(partial: "crags/travelling_climbers", formats: :html,
+                          locals: {crag: @crag})
