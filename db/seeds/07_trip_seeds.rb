423ed66d802461ab7e963582587364274b99070c
Ruben Voss <bekertshop@gmail.com>
2022-09-04 20:40:13 +0200

    trip seeds working

--- db/seeds/07_trip_seeds.rb
+++ db/seeds/07_trip_seeds.rb
@@ -1,11 +1,92 @@
-# def seed_trips
-#   puts "seeding trips..."
-#   10.times do
-#     crag = Crag.all.sample
-#     user = User.all.sample
-#     trip = Trip.new(name: crag.name, start_date: , end_date: , crag_id: crag.id, user_id: user.id)
-#     puts "trip with name #{trip.name} created"
-#   end
-# end
+trips_data = [
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Ruben")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Jasmien")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Max")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Theo")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Martin")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Connor")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Marie")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Anna")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Carla")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Shannon")[0].user
+  },
+  {
+    name: "Sending hard routes in Margalef",
+    crag: Crag.where(name: "Margalef, Spain")[0],
+    start_date: Date.new(2022, 9, 20),
+    end_date: Date.new(2022, 9, 30),
+    user: Profile.where(name: "Alice")[0].user
+  }
+]
 
-# seed_trips
+def seed_trips(trips_data)
+  puts "seeding trips..."
+  trips_data.each do |trip_data|
+    trip = Trip.new(name: trip_data[:name], start_date: trip_data[:start_date],
+                    end_date: trip_data[:end_date], crag: trip_data[:crag],
+                    user: trip_data[:user])
+    trip.save!
+    puts "trip with name #{trip.name} created"
+  end
+end
+
+seed_trips(trips_data)
