1770425c9527300774ab20e8def7f42b34148d94
Ruben Voss <bekertshop@gmail.com>
2022-09-04 18:40:48 +0200

    fixed admin seeds no profile issue

--- db/seeds/01_admin_seed.rb
+++ db/seeds/01_admin_seed.rb
@@ -1,6 +1,6 @@
-def seed_user
-  puts "seeding admin user - admin@admin.com - 123456"
-  admin_user = User.create(email: "admin@admin.com", password: "123456")
-end
+# def seed_user
+#   puts "seeding admin user - admin@admin.com - 123456"
+#   admin_user = User.create(email: "admin@admin.com", password: "123456")
+# end
 
-seed_user
+# seed_user
