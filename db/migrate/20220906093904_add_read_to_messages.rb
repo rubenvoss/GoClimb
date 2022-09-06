86d4476f9106039344e6d3a721034e9cde16dd9a
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-06 11:41:56 +0200

    added default false

--- db/migrate/20220906093904_add_read_to_messages.rb
+++ db/migrate/20220906093904_add_read_to_messages.rb
@@ -1,5 +1,5 @@
 class AddReadToMessages < ActiveRecord::Migration[7.0]
   def change
-    add_column :messages, :read, :boolean
+    add_column :messages, :read, :boolean, default: false
   end
 end
