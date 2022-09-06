43c530cf0158da6766623d6c618a5f48bb71dc14
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-06 11:55:54 +0200

    added notification channel

--- app/javascript/controllers/notification_subscription_controller.js
+++ app/javascript/controllers/notification_subscription_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="notification-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number }
+  
   connect() {
   }
 }
