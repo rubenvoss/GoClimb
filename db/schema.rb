1db6a660bc79b904a80104411a8ca29b3e3ac5e4
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-06 11:40:38 +0200

    undid migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_093904) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_104202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,8 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_093904) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "read"
-    t.string "boolean"
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
