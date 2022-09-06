a83d5b9fd3f68353739a2c3c9cdef70afc61bc3b
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-06 11:10:06 +0200

    chatroom names changed to user texted

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,8 +1,6 @@
 class Chatroom < ApplicationRecord
   has_many :messages
   has_many :users, through: :messages
-  validates :name, presence: true
-  validates :name, uniqueness: true
 
   def other_user(current_user)
     users.find{ |user| user != current_user } # users = self.users
