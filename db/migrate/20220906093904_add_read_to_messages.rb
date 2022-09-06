902abf592937e5a94f877706a0c6745919c04eba
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-06 11:41:09 +0200

    ran correct migration

--- db/migrate/20220906093904_add_read_to_messages.rb
+++ db/migrate/20220906093904_add_read_to_messages.rb
@@ -1,6 +1,5 @@
 class AddReadToMessages < ActiveRecord::Migration[7.0]
   def change
-    add_column :messages, :read, :string
-    add_column :messages, :boolean, :string
+    add_column :messages, :read, :boolean
   end
 end
