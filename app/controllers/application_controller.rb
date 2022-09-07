25ae134d9eb6206199404f1fe20e23dd1c80f576
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-07 18:22:20 +0200

    notifications works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+  before_action :clean_messages, if: :coming_from_chat?
 
   def storable_location?
    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
@@ -16,5 +17,15 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def coming_from_chat?
+    request&.referer&.match(/chatrooms\/\d+/)&.size&.positive?
+  end
+
+  def clean_messages
+    chatroom = Chatroom.find(request.referer.split("/").last)
+    other_user = chatroom.other_user(current_user)
+    current_user.mark_as_read_with(other_user)
   end
 end
