af502a53a0c7cf90a2afe3c68f66500c44ac0a43
Ruben Voss <bekertshop@gmail.com>
2022-09-07 15:19:17 +0200

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+
+  def storable_location?
+   request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+   store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+   stored_location_for(resource_or_scope) || super
+  end
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
--- db/seeds/04_seed_profiles.rb
+++ db/seeds/04_seed_profiles.rb
@@ -33,7 +33,8 @@ male_profiles = [
   {
     name: "Jasmien",
     url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662031176/go%20climb%20profile%20pictures/omid-armin-D9RrI5IW9h0-unsplash_50_1_50_joecin.jpg",
-    age: 29
+    bio: "I am quite new to climbing but I learn fast 😊. I love traveling and discovering new places all over the world.",
+    age: 29,
   },
   {
     name: "Max",
