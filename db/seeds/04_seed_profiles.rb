758d828750fa1625a9d2eeaa658589b35247d181
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-07 15:02:44 +0200

    deleted homecrag from seeds again

--- db/seeds/04_seed_profiles.rb
+++ db/seeds/04_seed_profiles.rb
@@ -35,7 +35,6 @@ male_profiles = [
     url: "https://res.cloudinary.com/dqdezmage/image/upload/v1662031176/go%20climb%20profile%20pictures/omid-armin-D9RrI5IW9h0-unsplash_50_1_50_joecin.jpg",
     bio: "I am quite new to climbing but I learn fast 😊. I love traveling and discovering new places all over the world.",
     age: 29,
-    crag: "Oberammergau"
   },
   {
     name: "Max",
