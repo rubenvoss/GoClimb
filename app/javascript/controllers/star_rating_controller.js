4e2d9c222ad2aee25930d0afa19865bdd773a3f9
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-14 21:36:13 +0200

    removed data target

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -3,10 +3,8 @@ import StarRating from "star-rating.js"
 
 // Connects to data-controller="star-rating"
 export default class extends Controller {
-  static targets = [ "stars" ]
 
   connect() {
     new StarRating(this.element),
-    new StarRating(this.starsTarget.value)
   }
 }
