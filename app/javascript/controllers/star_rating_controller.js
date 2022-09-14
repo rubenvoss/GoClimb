08ab9c719d8760cc9d1e80a90f02205e66f12053
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-09-14 21:20:58 +0200

    added data target: work in progress

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -3,7 +3,10 @@ import StarRating from "star-rating.js"
 
 // Connects to data-controller="star-rating"
 export default class extends Controller {
+  static targets = [ "stars" ]
+
   connect() {
-    new StarRating(this.element)
+    new StarRating(this.element),
+    new StarRating(this.starsTarget.value)
   }
 }
