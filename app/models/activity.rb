ce70f1b8ccd9a883649f93368052618db84661cb
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-10-04 16:24:12 +0200

    full words needed for geocoder gem

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :crag
-  validates :name, :lat, :long, presence: true
+  validates :name, :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/migrate/20221004140706_add_coordinates_to_activities.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToActivities < ActiveRecord::Migration[7.0]
-  def change
-    add_column :activities, :lat, :float
-    add_column :activities, :long, :float
-  end
-end
