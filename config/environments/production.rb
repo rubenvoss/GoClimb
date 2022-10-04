38bcae9ff37a9a65d5c4c6f1ec44dbd1ec177195
Ruben Voß <bekertshop@gmail.com>
2022-10-04 02:56:37 +0200

    add comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+  # right now, puma is rendering static files, but we can change this later
   config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
